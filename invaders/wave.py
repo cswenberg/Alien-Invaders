26d0f640ba7d53ebe437b577b231494c88c24d94
Conner Swenberg <cls364@cornell.edu>
2018-08-06 22:07:38 -0700

    inclusion of increase difficulty

--- invaders/wave.py
+++ invaders/wave.py
@@ -5,7 +5,7 @@ This module contains the subcontroller to manage a single level or wave in the A
 Invaders game.  Instances of Wave represent a single wave.  Whenever you move to a
 new level, you are expected to make a new instance of the class.
 
-The subcontroller Wave manages the ship, the aliens and any laser bolts on screen.  
+The subcontroller Wave manages the ship, the aliens and any laser bolts on screen.
 These are model objects.  Their classes are defined in models.py.
 
 Most of your work on this assignment will be in either this module or models.py.
@@ -21,44 +21,44 @@ from models import *
 import random
 
 # PRIMARY RULE: Wave can only access attributes in models.py via getters/setters
-# Wave is NOT allowed to access anything in app.py (Subcontrollers are not permitted 
+# Wave is NOT allowed to access anything in app.py (Subcontrollers are not permitted
 # to access anything in their parent. To see why, take CS 3152)
 
 
 class Wave(object):
     """
     This class controls a single level or wave of Alien Invaders.
-    
-    This subcontroller has a reference to the ship, aliens, and any laser bolts on screen. 
+
+    This subcontroller has a reference to the ship, aliens, and any laser bolts on screen.
     It animates the laser bolts, removing any aliens as necessary. It also marches the
     aliens back and forth across the screen until they are all destroyed or they reach
-    the defense line (at which point the player loses). When the wave is complete, you 
-    should create a NEW instance of Wave (in Invaders) if you want to make a new wave of 
+    the defense line (at which point the player loses). When the wave is complete, you
+    should create a NEW instance of Wave (in Invaders) if you want to make a new wave of
     aliens.
-    
-    If you want to pause the game, tell this controller to draw, but do not update.  See 
+
+    If you want to pause the game, tell this controller to draw, but do not update.  See
     subcontrollers.py from Lecture 24 for an example.  This class will be similar to
     than one in how it interacts with the main class Invaders.
-    
+
     #UPDATE ME LATER
     INSTANCE ATTRIBUTES:
         _ship:   the player ship to control [Ship]
-        _aliens: the 2d list of aliens in the wave [rectangular 2d list of Alien or None] 
+        _aliens: the 2d list of aliens in the wave [rectangular 2d list of Alien or None]
         _bolts:  the laser bolts currently on screen [list of Bolt, possibly empty]
         _dline:  the defensive line being protected [GPath]
         _lives:  the number of lives left  [int >= 0]
         _time:   The amount of time since the last Alien "step" [number >= 0]
-    
+
     As you can see, all of these attributes are hidden.  You may find that you want to
-    access an attribute in class Invaders. It is okay if you do, but you MAY NOT ACCESS 
-    THE ATTRIBUTES DIRECTLY. You must use a getter and/or setter for any attribute that 
-    you need to access in Invaders.  Only add the getters and setters that you need for 
+    access an attribute in class Invaders. It is okay if you do, but you MAY NOT ACCESS
+    THE ATTRIBUTES DIRECTLY. You must use a getter and/or setter for any attribute that
+    you need to access in Invaders.  Only add the getters and setters that you need for
     Invaders. You can keep everything else hidden.
-    
-    You may change any of the attributes above as you see fit. For example, may want to 
+
+    You may change any of the attributes above as you see fit. For example, may want to
     keep track of the score.  You also might want some label objects to display the score
     and number of lives. If you make changes, please list the changes with the invariants.
-    
+
     LIST MORE ATTRIBUTES (AND THEIR INVARIANTS) HERE IF NECESSARY
         _moveright: determines if aliens should move right [True if yes, False if not]
         _firerate: random number between 1 and BOLT_RATE, changes after each new alien bolt created
@@ -73,33 +73,33 @@ class Wave(object):
         _tribolt: determine if powerup tribolt is engaged
         _invincible: determine if powerup invincible is engaged
     """
-    
+
     # GETTERS AND SETTERS (ONLY ADD IF YOU NEED THEM)
     def getState(self):
         """
         """
         return self._wavestate
-    
+
     def setState(self,state):
         """
         """
         self._wavestate=state
-    
+
     def getAliens(self):
         """
         """
         return self._aliencounter
-    
+
     def getScore(self):
         """
         """
         return self._score
-    
+
     def getLives(self):
         """
         """
         return self._lives
-    
+
     def getPowerup(self):
         """
         """
@@ -109,13 +109,13 @@ class Wave(object):
             return 'Invincible: '+str(round(self._invincibletime,1))
         else:
             return '            '
-    
+
     def getWavesleft(self):
         """
         Returns: the number of waves left to win the game
         """
         return self._wavesleft
-    
+
     # INITIALIZER (standard form) TO CREATE SHIP AND ALIENS
     def __init__(self, lives=SHIP_LIVES, waves=ALIEN_WAVES, score=0):
         self._aliens=self.alienlist()
@@ -137,8 +137,8 @@ class Wave(object):
         self._poweruptime=random.randint(FIRST_POWERUP,ALIEN_ROWS*ALIENS_IN_ROW-1)
         self._triboltshots=0
         self._invincibletime=0
-        
-        
+
+
     def alienlist(self):
         """
         helper for __init__ to make 2D list of aliens
@@ -158,7 +158,7 @@ class Wave(object):
                 n=0
                 m=0
         return alienlist
-    
+
     # UPDATE METHOD TO MOVE THE SHIP, ALIENS, AND LASER BOLTS
     def update(self,view,state,inp,dt):
         if self._wavestate==STATE_ACTIVE:
@@ -166,7 +166,7 @@ class Wave(object):
                 self._ship.moveship(inp)
             self.makebolt(inp)
             if len(self._bolts)>0 and self._aliencounter>0:
-                self.boltupdate()    
+                self.boltupdate()
             self.powerupupdate(dt)
             self._time=self._time+dt
             self.movealiens(self._newalienspeed - (WAVE_SPEED_INCREASE*(ALIEN_WAVES-self._wavesleft)))
@@ -174,12 +174,12 @@ class Wave(object):
                 self._wavestate=STATE_COMPLETE
             if self._lives<=0:
                 self._wavestate=STATE_COMPLETE
-                self._ship=None   
+                self._ship=None
         if state==STATE_CONTINUE:
             self._wavestate=STATE_ACTIVE
             self._ship=Ship()
-        
-    
+
+
     # DRAW METHOD TO DRAW THE SHIP, ALIENS, DEFENSIVE LINE AND BOLTS
     def draw(self,view):
         for row in self._aliens:
@@ -194,8 +194,8 @@ class Wave(object):
                 bolt.draw(view)
         for powerup in self._powerups:
             powerup.draw(view)
-        
-   
+
+
     #HELPER METHODS FOR MOVING ALIENS
     def movealiens(self, speed):
         """
@@ -220,7 +220,7 @@ class Wave(object):
             self.movedirection('right')
         elif not self._moveright and self._time>=speed:
             self.movedirection('left')
-    
+
     def findrightedge(self):
         """
         Returns: the rightmost edge of the alien wave
@@ -257,7 +257,7 @@ class Wave(object):
                     leftpos=row[col].getX()-ALIEN_WIDTH//2
                     return leftpos
         return leftpos
-    
+
     def movedirection(self,direction,xchange=ALIEN_H_WALK,ychange=ALIEN_V_WALK):
         """
         A helper method that moves all living aliens in a single direction.
@@ -266,7 +266,7 @@ class Wave(object):
 
         Parameter direction: the direction to move the aliens
         Precondition: direction is a string, either ?down?, ?right?, or ?left?
-    
+
         Parameter xchange: the distance for the aliens to move in the x direction
         Precondition: xchange is an integer, 0<xchange<GAME_WIDTH
 
@@ -288,12 +288,12 @@ class Wave(object):
                     if direction=='right':
                         alien.setX(alienx+xchange)
                         self._aliencounter=self._aliencounter+1
-                    if direction=='left':                    
+                    if direction=='left':
                         alien.setX(alienx-xchange)
                         self._aliencounter=self._aliencounter+1
         self._time=0
         self._stepcount+=1
-        
+
     #HELPER METHODS FOR CREATING AND UPDATING BOLTS
     def chooseshooter(self):
         """
@@ -308,18 +308,18 @@ class Wave(object):
             if row[col]!=None:
                 shooter=row[col]
                 return shooter
-        if self._aliencounter!=0:    
+        if self._aliencounter!=0:
             return self.chooseshooter()
         return None
-        
+
     def makebolt(self,inp):
         """
         Creates bolts from both the ship and the aliens.
 
-        This function determines whether or not a ship bolt should be made by looking at user input, and whether or not alien bolts should be fired. The parameter inp represents user input, and it is provided as an argument when it is called by update (which takes inp as an argument when called by the update in app.py). 
+        This function determines whether or not a ship bolt should be made by looking at user input, and whether or not alien bolts should be fired. The parameter inp represents user input, and it is provided as an argument when it is called by update (which takes inp as an argument when called by the update in app.py).
 
         Alien bolts depend on the bolt_rate, the number of steps the aliens have taken, and which alien should be firing. This method takes them all into account, and uses the helper method chooseshooter to determine which alien should be firing.
-        
+
         Parameter inp: the user input
         Precondition: inp is an instance of GInput
 
@@ -342,11 +342,11 @@ class Wave(object):
             self._stepcount=0
             self._firerate=random.randint(1,int(round(self._newboltrate)))
             self.playSound('alienbolt')
-            
+
     def boltupdate(self):
         """
         Updates the positions of all the bolts in the lisrt of bolts on screen.
-        
+
         Does this by looping through list of bolts and getting position and velocity
         to set a new y value.  Also calls checkcollision to check bolt collisions.
         """
@@ -357,10 +357,10 @@ class Wave(object):
             bolty+=bolt.getBoltVelo()
             bolt.setBoltY(bolty)
         self.checkcollision()
-        
+
     def checkcollision(self):
         """
-        A method to check if any bolts fired from aliens collide with the ship 
+        A method to check if any bolts fired from aliens collide with the ship
 
         This method simultaneously tracks collisions and which bolts are fired from the ship.  If a collision between an alien bolt and the ship is detected, The ship loses a life, the ship object is set to none,the bolt is removed from the list of bolts, Exploding sound made, the wave is paused, a penalty to score is applied.  Upon revival, the ship is granted a short period of invincibility time to recover.
 
@@ -384,14 +384,14 @@ class Wave(object):
                 self._score=self._score-SCORE_PENALTY
                 self._triboltshots=0
         self.checkaliencollision(shipbolts)
-            
+
     def checkaliencollision(self,shipbolts):
         """
         A method to check if any bolts fired from player collide with aliens.
 
-        If a collision is detected between a player bolt and alien, the player?s score is increased 
+        If a collision is detected between a player bolt and alien, the player?s score is increased
         (calls increase_score), a powerup is potentially dropped, the alien object is set to None,
-        The bolt is removed from the list of bolts on screen, and a popping sound is made.  
+        The bolt is removed from the list of bolts on screen, and a popping sound is made.
 
         Parameter shipbolts: the list of bolts fired from the ship
         Precondition: shipbolts is a list, each bolt has a velocity>0
@@ -406,6 +406,7 @@ class Wave(object):
                             check=self._aliens[numrow][numalien].boltcollides(bolt)
                         if check and self._aliens[numrow][numalien]!=None:
                             self.increase_score(self._aliens[numrow][numalien])
+                            self.increase_difficulty()
                             self.dropPowerup(self._aliens[numrow][numalien])
                             self._aliens[numrow][numalien]=None
                             del self._bolts[self._bolts.index(bolt)]
@@ -426,17 +427,17 @@ class Wave(object):
                 self._score=self._score+20
             if alien.source=='alien3.png':
                 self._score=self._score+40
-    
+
     def increase_difficulty(self):
         """
         Makes aliens move faster by using a speed multiplier after each alien is blasted.
         Also increases the frequency of bolts shot by aliens.
         """
-        if self._newalienspeed>ALIEN_MAX_SPEED:    
+        if self._newalienspeed>ALIEN_MAX_SPEED:
             self._newalienspeed*=ALIEN_SPEED_INCREASE
         if self._newboltrate>BOLT_MAX_SPEED:
             self._newboltrate*=BOLT_SPEED_INCREASE
-    
+
     def playSound(self,makesound):
         if makesound=='shipexplode':
             sound=Sound('blast1.wav')
@@ -450,7 +451,7 @@ class Wave(object):
             sound=Sound('pop2.wav')
         if makesound!=None:
             sound.play()
-        
+
     def dropPowerup(self,alien):
         if self._aliencounter==self._poweruptime:
             n=random.randint(0,len(POWERUP_IMAGE)-1)
@@ -460,7 +461,7 @@ class Wave(object):
             powertype=self._powerups[-1].getType()
             if self._aliencounter>1:
                 self._poweruptime=random.randint(0,self._aliencounter//2)
-    
+
     def powerupupdate(self,dt):
         if len(self._powerups)>0:
             for powerup in self._powerups:
